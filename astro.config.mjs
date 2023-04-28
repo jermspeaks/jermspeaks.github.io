54ad1fd86d1370267a650f778a526f84e3a1e035
jermspeaks <jeremy.wong@clearlabs.com>
2023-04-27 21:38:06 -0700

    Updated netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://craftbyzen.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://craftbyzen.com",
+  integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
