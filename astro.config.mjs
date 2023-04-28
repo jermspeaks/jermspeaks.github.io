5788bade6030fb95ab268fa3b815ce39fd8cc252
jermspeaks <jeremy.wong@clearlabs.com>
2023-04-28 15:54:25 -0700

    Add Svelte demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://craftbyzen.com",
-  integrations: [mdx(), sitemap()],
-  adapter: netlify(),
+  integrations: [mdx(), sitemap(), svelte()],
+  adapter: netlify()
 });
