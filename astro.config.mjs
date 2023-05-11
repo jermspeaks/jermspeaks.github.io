db81e7d380cc095edf17c225a251c31d55c00a4b
jermspeaks <jeremy.wong@clearlabs.com>
2023-05-11 13:16:06 -0700

    Add relative links link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
+import { autolinkConfig } from "./plugins/rehype-autolink-config";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://craftbyzen.com",
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      // This adds links to headings
+      [rehypeAutolinkHeadings, autolinkConfig],
+    ],
+  },
   integrations: [
     mdx(),
     sitemap(),
