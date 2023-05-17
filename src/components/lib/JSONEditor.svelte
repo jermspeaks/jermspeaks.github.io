568a785322f9f8545389d5cfd719ad042aa4fe28
jermspeaks <jeremy.wong@clearlabs.com>
2023-05-17 17:28:04 -0400

    Init for adding new project for a code editor

---
+++ src/components/lib/JSONEditor.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { json } from "@codemirror/lang-json";
+  import CodeEditor from "./CodeEditor.svelte";
+
+  let value = "";
+  const convert = () => {
+    try {
+      const obj = JSON.parse(value);
+      value = JSON.stringify(obj, null, 2);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+</script>
+
+<CodeEditor bind:value lang={json()} />
+
+<button
+  class="border-black border-solid border-2 bg-none cursor-pointer mt-8 flex justify-center p-1 text-black hover:text-gray-600"
+  on:click={convert}>Convert</button
+>
---
+++ src/components/lib/Temperature.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  let c = 20;
+  let f = 68;
+
+  function setBothFromC(value: number): void {
+    c = +value;
+    f = +(32 + (9 / 5) * c).toFixed(1);
+  }
+
+  function setBothFromF(value: number): void {
+    f = +value;
+    c = +((5 / 9) * (f - 32)).toFixed(1);
+  }
+</script>
+
+<!-- https://eugenkiss.github.io/7guis/tasks/#temp -->
+<input
+  value={c}
+  on:input={(e) => setBothFromC(e?.target?.value)}
+  type="number"
+  class="w-16"
+/>
+°C =
+<input
+  value={f}
+  on:input={(e) => setBothFromF(e?.target?.value)}
+  type="number"
+  class="w-16"
+/>
+°F
