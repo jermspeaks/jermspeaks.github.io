3d467165cfe0fcaa926df30568f5c68689515dfb
jermspeaks <jeremy.wong@clearlabs.com>
2024-02-08 21:37:44 -0800

    Finished the Svelte article

---
+++ src/components/lib/Animation.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { slide } from "svelte/transition";
+  let items = ["Item 1", "Item 2", "Item 3"];
+
+  function addItem() {
+    items = [...items, `Item ${items.length + 1}`];
+  }
+
+  function removeItem(index) {
+    items = items.filter((_, i) => i !== index);
+  }
+</script>
+
+<button
+  class="my-2 px-2 border-slate-800 dark:border-slate-200 border-2 rounded hover:bg-purple-500"
+  on:click={addItem}>Add Item</button
+>
+
+<ul class="list-none">
+  {#each items as item, index (item)}
+    <li transition:slide={{ duration: 200 }}>
+      {item}
+      <button
+        class="my-2 px-2 border-slate-800 dark:border-slate-200 border-2 rounded hover:bg-purple-500"
+        on:click={() => removeItem(index)}>Remove</button
+      >
+    </li>
+  {/each}
+</ul>
---
+++ src/components/lib/CounterButtons.svelte
@@ -0,0 +1,30 @@
+<script>
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+
+  function decrement() {
+    count -= 1;
+  }
+</script>
+
+<main>
+  <h3>The count is {count}</h3>
+  <button
+    class="my-2 border-slate-800 dark:border-slate-200 border-2 rounded hover:bg-purple-500"
+    on:click={decrement}>-</button
+  >
+  <button
+    class="my-2 border-slate-800 dark:border-slate-200 border-2 rounded hover:bg-purple-500"
+    on:click={increment}>+</button
+  >
+
+  {#if count > 10}
+    <p>You have reached a count greater than 10!</p>
+  {/if}
+</main>
+
+<style>
+</style>
